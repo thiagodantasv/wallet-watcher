ff6acbe68a0b79179d901ad075069ae0644ceaac
Thiago Dantas <thiagodantasv@gmail.com>
2021-02-23 00:23:59 -0300

    Starting of the wallet watcher web project

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('testing index ts');
+let port = 3000;
+console.log(port);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,3 @@
+console.log('testing index ts');
+let port = 3000;
+console.log(port);
